e406e3b8a21141b269478a04ab134a2c02f4f91f
Adrian Denkiewicz <adrian.denkiewicz@gmail.com>
2019-04-28 16:26:44 +0200

    GMON: working exploit

---
+++ GMON/fuzz.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from boofuzz import *
+
+
+def main():
+    session = Session(
+            target=Target(connection=SocketConnection("192.168.0.102", 9999, proto='tcp')),
+            )
+
+    s_initialize(name="Request")
+    # s_group("Command", ["STATS", "RTIME", "LTIME", "SRUN", "TRUN", "GMON", "GDOG", "KSTET", "GTER", "HTER", "LTER", "KSTAN"])
+    s_static("GMON")
+    with s_block("arg"):
+        s_delim(" ", fuzzable=False, name='space-1')
+        s_string("1")
+    s_repeat("arg", min_reps=1, max_reps=5)
+    s_static("\n")
+
+    session.connect(s_get("Request"))
+
+    session.fuzz()
+
+
+if __name__ == "__main__":
+    main()
